e738919df00b4b74f21f1cb31f8ccb5dc904e2fc
Babak Shokouhi Pour <b.shokouhipour@zeema.fund>
2026-02-27 23:13:31 +0330

    feat: add CLI interface with add, list, done, delete commands

--- main.go
+++ main.go
@@ -3,20 +3,84 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/theusualdeveloper/task-manager/task"
 )
 
 func main() {
-	ts := task.NewTaskStore()
-	ts.Add("First Task")
-	ts.Add("Second Task")
-	ts.Add("Third Task")
-
-	err := ts.Complete(2)
-	if err != nil {
-		fmt.Println("Error:", err)
+	args := os.Args
+	if len(args) <= 1 {
+		showCommandGuidelines()
 		os.Exit(1)
 	}
-	fmt.Println(ts.List())
+	command := args[1]
+	ts := task.NewTaskStore()
+	switch command {
+	case "add":
+		cs := args[2:]
+		if len(cs) == 0 {
+			fmt.Println("Please provide task content")
+			os.Exit(1)
+		}
+		c := strings.Join(cs, " ")
+		task := ts.Add(c)
+		fmt.Printf("Task added: [%d] %s\n", task.ID, task.Title)
+	case "list":
+		tasks := ts.List()
+		var done string
+		for _, task := range tasks {
+			if task.Done {
+				done = "done"
+			} else {
+				done = "pending"
+			}
+			fmt.Printf("[%d] %s    (%s)\n", task.ID, task.Title, done)
+		}
+	case "done":
+		cs := args[2:]
+		if len(cs) == 0 {
+			fmt.Println("Please provide task id")
+			os.Exit(1)
+		}
+		id, err := strconv.Atoi(cs[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = ts.Complete(id)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf("Task %d marked as done.\n", id)
+	case "delete":
+		cs := args[2:]
+		if len(cs) == 0 {
+			fmt.Println("Please provide task id")
+			os.Exit(1)
+		}
+		id, err := strconv.Atoi(cs[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = ts.Delete(id)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf("Task %d deleted.\n", id)
+	default:
+		showCommandGuidelines()
+	}
+}
+
+func showCommandGuidelines() {
+	fmt.Println("Usage:")
+	fmt.Println(" add <title>")
+	fmt.Println(" list")
+	fmt.Println(" done <id>")
+	fmt.Println(" delete <id>")
 }
